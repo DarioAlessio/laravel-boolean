ff7d4e56167f0abbf19a07f95a7ae274637cd990
Dario <darioalessio79@gmail.com>
2021-01-22 16:51:52 +0100

    compact...

--- laravel/app/Http/Controllers/MainController.php
+++ laravel/app/Http/Controllers/MainController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function test() {
-      return view('start3');
-    }
+    // public function test() {
+    //   return view('start3');
+    // }
     public function test2() {
-      return view('start3',['saluto' => 'todosss']);
+
+      $saluto = "Hermanooo";
+
+      return view  ('start3', compact('saluto'));
     }
     public function test3() {
       return view('start3',[
-        'saluto' => 'todosss',
-        'name' => 'Pablo Escobar!!!'
+        'saluto' => 'todosss'
       ]);
     }
 }
